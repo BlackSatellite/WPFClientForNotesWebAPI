106fd380bcc2778ae6dccdfd991742866a3ce05d
John <wesatellite@bigmir.net>
2023-10-24 02:08:22 +0300

    Some Client for Note

---
+++ WPFClientForNotesWebAPI/Models/CreateNoteDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace WPFClientForNotesWebAPI.Models
+{
+    public class CreateNoteDto
+    {
+        public string Title { get; set; }
+        public string Details { get; set; }
+    }
+}
